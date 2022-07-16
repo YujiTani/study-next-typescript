916947729caffa8c92bc0caaddf4d542b50ec806
Yuji Tani <tani@leanonme.co.jp>
2022-07-16 19:10:00 +0900

    型推論、型アノテーション、型アサーションを学んだ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,22 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const foo : number = 123
+  let baz = {} as { bar: number }; // 型アサーション
+  // let baz = <{ bar: number }>{}; // jsxファイルなどでは、こんな風に記述する
+
+  baz.bar = 1;
+
+  const double = (x: number ): number | undefined => {
+    if (x > 0) {
+      return
+    }
+
+    return x * 2
+  }
+
+  double(foo)
+
   return (
       <div>Test</div>
   )
