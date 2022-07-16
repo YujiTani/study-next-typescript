12eb93d915aeec4495580bb706d872c18b29cb20
Yuji Tani <tani@leanonme.co.jp>
2022-07-17 01:48:30 +0900

    インタークロスとユニオン型について学んだ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,24 @@
 import type { NextPage } from 'next'
 
-let obj1: {} = 1 // プリミティブ型でもなんでも受け入れてしまう
-let obj2: object = [1,2,3,4] // 非プリミティブ型を受けれちゃうので良くない
-let obj3: Record<string, unknown> = {}
-let obj4: {[key: string]: unknown} = {} // 3,4は同じような書き方ができるがしまぶーさんはこっち推し
-let obj5: {string: string; foo?: string; number: number} = {
-  string: "string",
-  number: 3
-} 
-let obj6: {
-  object: 
-    {
-    type: string;
-    key: string;
-    foo: number;
-    bar: boolean
-    }
-  } = {
-  object: {
-    type: "aaa",
-    key: "test",
-    foo: 1,
-    bar: true
-  }
-} 
+type foo = {
+  name: "テキサスバーガー"
+}
 
+type bar = {
+  name: "ハリウッドバーガー"
+}
+
+type fooBar = foo | bar 
+
+const test: fooBar = {
+  name: "テキサスバーガー"
+}
+
+if (test.name = "テキサスバーガー") {
+ const res = true
+} else {
+  const res = false
+}
 
 function Home() {
   return (
