ff7422c460c1695078b1b264b92ce315e769c34f
Yuji Tani <tani@leanonme.co.jp>
2022-07-16 23:06:46 +0900

    typescriptでオブジェクト型の作り方(難しい)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,30 @@
 import type { NextPage } from 'next'
 
-// Arrayの書き方
-const foo : (number | string )[] = [1,"2","3",1,3]
-const bar : [string, number, boolean] = ["1",2,false]
+let obj1: {} = 1 // プリミティブ型でもなんでも受け入れてしまう
+let obj2: object = [1,2,3,4] // 非プリミティブ型を受けれちゃうので良くない
+let obj3: Record<string, unknown> = {}
+let obj4: {[key: string]: unknown} = {} // 3,4は同じような書き方ができるがしまぶーさんはこっち推し
+let obj5: {string: string; foo?: string; number: number} = {
+  string: "string",
+  number: 3
+} 
+let obj6: {
+  object: 
+    {
+    type: string;
+    key: string;
+    foo: number;
+    bar: boolean
+    }
+  } = {
+  object: {
+    type: "aaa",
+    key: "test",
+    foo: 1,
+    bar: true
+  }
+} 
 
-type baz = () => void 
-
-const testFunc: baz = () => {
-  alert('テスト')
-}
 
 function Home() {
   return (
